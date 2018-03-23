20307cc6c57a4f70ed80cff32a18b8bfcfbad6e9
NikitaDolgov <nikitdolgov@yandex.ru>
2018-03-23 15:51:15 +0300

    added intro button

--- calendar-app/src/components/SideBlock.js
+++ calendar-app/src/components/SideBlock.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Checkbox, Button } from 'antd';
+import { Checkbox, Button, Popover } from 'antd';
 import './SideBlock.css';
 import Month from './Month';
 
 const ButtonGroup = Button.Group;
+const content = (
+    <div>
+      <p>1. Дни месяцев - кнопки</p>
+      <p>2. Поп-ап при нажатии на день месяца</p>
+      <p>3. Переключение вью при нажатии кнопок "День", "Неделя"...</p>
+      <p>4. Автоматическое выделение текущей даты синим</p>
+      <p>5. Два чекбокса</p>
+      <p>6. Инпут поле для поиска</p>
+    </div>
+  );
 
 class SideBlock extends React.Component {
     render (){
@@ -13,7 +23,12 @@ class SideBlock extends React.Component {
                 <div class='calendars'>
                     <div><Checkbox className='SideBlock__checkbox'>Holidays in Russia</Checkbox></div>
                     <div><Checkbox className='SideBlock__checkbox'>t.shapotko@directual.com</Checkbox></div>
-                </div>
+                    <div class='todel'>
+                        <Popover content={content} title="Я могу" trigger="click">
+                            <Button className='button-to-del'>Нажми меня!</Button>
+                        </Popover>
+                    </div> 
+                </div>  
                 <hr class='line'></hr>
                 <div class='small-month'>
                     <div class='SideBlock__month-name'> 
